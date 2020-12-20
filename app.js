825d94a479cb8389a39202f55e58d694538619d0
Barış Gündüz <baris.gunduz@devstup.org>
2020-12-20 16:39:52 +0300

    Registiration and login pages

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const middleware = require('./middleware')
+const path = require('path');
+const bodyParser = require("body-parser");
 
 const server = app.listen(port, () => {
     console.log("server listening on port " + port);
@@ -9,7 +12,19 @@ const server = app.listen(port, () => {
 app.set("view engine", "pug");
 app.set("views", "views");
 
-app.get("/", (req, res, next) => {
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+// Routes
+const loginRoute = require('./routes/loginRoutes');
+const registerRoute = require('./routes/registerRoutes');
+
+app.use("/login", loginRoute);
+app.use("/register", registerRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
 
     var payload = {
         pageTitle: "Home"
