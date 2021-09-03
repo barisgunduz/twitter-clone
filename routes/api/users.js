c5c7920bbc44068c168a83179e3eb94e6950ee48
Barış Gündüz <baris.gunduz@devstup.org>
2021-09-03 14:32:16 +0300

    Uploading profile pictures

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const router = express.Router();
 const bodyParser = require("body-parser");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const upload = multer({ dest: "uploads/" });
 const User = require("../../schemas/UserSchema");
 const Post = require("../../schemas/PostSchema");
 
@@ -39,7 +43,7 @@ router.put("/:userId/follow", async (req, res, next) => {
 
 router.get("/:userId/following", async (req, res, next) => {
     User.findById(req.params.userId)
-    .populate("following")
+        .populate("following")
         .then((results) => {
             res.status(200).send(results);
         })
@@ -51,7 +55,7 @@ router.get("/:userId/following", async (req, res, next) => {
 
 router.get("/:userId/followers", async (req, res, next) => {
     User.findById(req.params.userId)
-    .populate("followers")
+        .populate("followers")
         .then((results) => {
             res.status(200).send(results);
         })
@@ -60,5 +64,34 @@ router.get("/:userId/followers", async (req, res, next) => {
             res.sendStatus(400);
         });
 });
+
+router.post(
+    "/profilePicture",
+    upload.single("croppedImage"),
+    async (req, res, next) => {
+        if (!req.file) {
+            console.log("No file uploaded with ajax request.");
+            return res.sendStatus(400);
+        }
+
+        var filePath = `/uploads/images/${req.file.filename}.png`;
+        var tempPath = req.file.path;
+        var targetPath = path.join(__dirname, `../../${filePath}`);
+
+        fs.rename(tempPath, targetPath, async (error) => {
+            if (error != null) {
+                console.log(error);
+                return res.sendStatus(400);
+            }
+
+            req.session.user = await User.findByIdAndUpdate(
+                req.session.user._id,
+                { profilePic: filePath },
+                { new: true }
+            );
+            res.sendStatus(204);
+        });
+    }
+);
 
 module.exports = router;
