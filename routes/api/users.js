b2fee7ebdb6edd1552cf00d7199e5c81e49295d8
Barış Gündüz <baris.gunduz@devstup.org>
2021-09-03 16:34:17 +0300

    Uploading a cover photo

--- routes/api/users.js
+++ routes/api/users.js
@@ -65,10 +65,7 @@ router.get("/:userId/followers", async (req, res, next) => {
         });
 });
 
-router.post(
-    "/profilePicture",
-    upload.single("croppedImage"),
-    async (req, res, next) => {
+router.post("/profilePicture", upload.single("croppedImage"), async (req, res, next) => {
         if (!req.file) {
             console.log("No file uploaded with ajax request.");
             return res.sendStatus(400);
@@ -87,6 +84,32 @@ router.post(
             req.session.user = await User.findByIdAndUpdate(
                 req.session.user._id,
                 { profilePic: filePath },
+                { new: true }
+            );
+            res.sendStatus(204);
+        });
+    }
+);
+
+router.post("/coverPhoto", upload.single("croppedImage"), async (req, res, next) => {
+        if (!req.file) {
+            console.log("No file uploaded with ajax request.");
+            return res.sendStatus(400);
+        }
+
+        var filePath = `/uploads/images/${req.file.filename}.png`;
+        var tempPath = req.file.path;
+        var targetPath = path.join(__dirname, `../../${filePath}`);
+
+        fs.rename(tempPath, targetPath, async (error) => {
+            if (error != null) {
+                console.log(error);
+                return res.sendStatus(400);
+            }
+
+            req.session.user = await User.findByIdAndUpdate(
+                req.session.user._id,
+                { coverPhoto: filePath },
                 { new: true }
             );
             res.sendStatus(204);
